920c154aeb763a1ae6974186d55b093d27a0b4d8
Jack Sorrell <jack@jacksorrell.com>
2021-07-16 17:21:08 -0400

    Fix Mod ID incorrectly changed by IDE refactor

--- src/template/java/com/jsorrell/skyblock/Build.java
+++ src/template/java/com/jsorrell/skyblock/Build.java
@@ -1,7 +1,7 @@
 package com.jsorrell.skyblock;
 
 public final class Build {
-  public static final String ID = "com.jsorrell.skyblock";
+  public static final String ID = "skyblock";
   public static final String NAME = "SkyBlock";
   public static final String VERSION = "${version}";
   public static final String BUILD_TIMESTAMP = "${timestamp}";
