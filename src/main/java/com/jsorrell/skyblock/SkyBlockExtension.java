a1f5fe092e2255de08b779defd5adcb58f18bf55
Jack Sorrell <jack@jacksorrell.com>
2022-02-15 17:44:37 -0500

    Fix Piglin Brutes spawning in midair

--- src/main/java/com/jsorrell/skyblock/SkyBlockExtension.java
+++ src/main/java/com/jsorrell/skyblock/SkyBlockExtension.java
@@ -3,13 +3,16 @@ package com.jsorrell.skyblock;
 import carpet.CarpetExtension;
 import carpet.CarpetServer;
 import carpet.settings.SettingsManager;
-import net.fabricmc.api.ModInitializer;
-
-import net.minecraft.util.registry.Registry;
-
 import com.jsorrell.skyblock.criterion.Criteria;
 import com.jsorrell.skyblock.gen.SkyBlockChunkGenerator;
 import com.jsorrell.skyblock.gen.SkyBlockGenerationSettings;
+import com.jsorrell.skyblock.mixin.SpawnRestrictionAccessor;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.mob.PiglinBruteEntity;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.Heightmap;
 
 public class SkyBlockExtension implements CarpetExtension, ModInitializer {
   private static SettingsManager settingsManager;
@@ -22,6 +25,8 @@ public class SkyBlockExtension implements CarpetExtension, ModInitializer {
   public void onInitialize() {
     settingsManager = new SettingsManager(Build.VERSION, Build.ID, Build.NAME);
     settingsManager.parseSettingsClass(SkyBlockSettings.class);
+    // Restrict Piglin Brute spawning to the ground
+    SpawnRestrictionAccessor.register(EntityType.PIGLIN_BRUTE, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, PiglinBruteEntity::canSpawnInDark);
     Registry.register(
         Registry.CHUNK_GENERATOR, SkyBlockGenerationSettings.NAME, SkyBlockChunkGenerator.CODEC);
     Criteria.registerAll();
