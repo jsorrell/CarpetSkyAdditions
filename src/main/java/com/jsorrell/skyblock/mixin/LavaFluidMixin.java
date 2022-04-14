fc748d900c0dfc7133e8bcf7633101c4764a501d
Jack Sorrell <jack@jacksorrell.com>
2022-04-14 03:18:29 -0400

    Add a sound when Budding Amethyst are generated.

--- src/main/java/com/jsorrell/skyblock/mixin/LavaFluidMixin.java
+++ src/main/java/com/jsorrell/skyblock/mixin/LavaFluidMixin.java
@@ -1,25 +1,25 @@
 package com.jsorrell.skyblock.mixin;
 
-import java.util.Iterator;
-import java.util.Random;
-
+import com.jsorrell.skyblock.SkyBlockSettings;
+import com.jsorrell.skyblock.criterion.Criteria;
+import com.jsorrell.skyblock.helpers.GeodeGenerator;
+import net.minecraft.block.Blocks;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.LavaFluid;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import net.minecraft.block.Blocks;
-import net.minecraft.fluid.FluidState;
-import net.minecraft.fluid.LavaFluid;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
-import com.jsorrell.skyblock.criterion.Criteria;
-import com.jsorrell.skyblock.SkyBlockSettings;
-import com.jsorrell.skyblock.helpers.GeodeGenerator;
+import java.util.Iterator;
+import java.util.Random;
 
 @Mixin(LavaFluid.class)
 public class LavaFluidMixin {
@@ -30,6 +30,8 @@ public class LavaFluidMixin {
       if (random.nextInt(GeodeGenerator.CONVERSION_RATE) == 0) {
         if (GeodeGenerator.checkGeodeFormation(world, pos)) {
           world.setBlockState(pos, Blocks.BUDDING_AMETHYST.getDefaultState());
+          world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BLOCK_LAVA_EXTINGUISH, SoundCategory.BLOCKS, 0.5f, 2.6f + (random.nextFloat() - random.nextFloat()) * 0.8f);
+          world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BLOCK_AMETHYST_BLOCK_PLACE, SoundCategory.BLOCKS, 1.0f, 0.5f + world.random.nextFloat() * 1.2f);
 
           Iterator<ServerPlayerEntity> nearbyPlayers =
               world
