dce78d85e24b42d0c102facf902eb0fc078847f0
Jack Sorrell <jack@jacksorrell.com>
2022-04-14 02:50:43 -0400

    Disallow bonemealing saplings on sand

--- src/main/java/com/jsorrell/skyblock/mixin/SaplingBlockMixin.java
+++ src/main/java/com/jsorrell/skyblock/mixin/SaplingBlockMixin.java
@@ -9,6 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
 
@@ -17,6 +18,11 @@ public abstract class SaplingBlockMixin extends PlantBlock {
 
   public SaplingBlockMixin(Settings settings) {
     super(settings);
+  }
+
+  private boolean saplingIsOnSand(BlockView world, BlockPos pos) {
+    BlockState underBlock = world.getBlockState(pos.down());
+    return underBlock.isOf(Blocks.SAND) || underBlock.isOf(Blocks.RED_SAND);
   }
 
   @Override
@@ -33,14 +39,22 @@ public abstract class SaplingBlockMixin extends PlantBlock {
       cancellable = true
   )
   private void killIfOnSand(BlockState blockState, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
-    if (SkyBlockSettings.saplingsDieOnSand) {
-      BlockState underBlock = world.getBlockState(pos.down());
-      if (underBlock.isOf(Blocks.SAND) || underBlock.isOf(Blocks.RED_SAND)) {
-        if (random.nextFloat() < 0.2) {
-          world.setBlockState(pos, Blocks.DEAD_BUSH.getDefaultState(), Block.NOTIFY_ALL);
-        }
-        ci.cancel();
+    if (SkyBlockSettings.saplingsDieOnSand && saplingIsOnSand(world, pos)) {
+      if (random.nextFloat() < 0.2) {
+        world.setBlockState(pos, Blocks.DEAD_BUSH.getDefaultState(), Block.NOTIFY_ALL);
       }
+      ci.cancel();
+    }
+  }
+
+  @Inject(
+      method = "isFertilizable",
+      at = @At("HEAD"),
+      cancellable = true
+  )
+  private void stopBonemealingOnSand(BlockView world, BlockPos pos, BlockState state, boolean isClient, CallbackInfoReturnable<Boolean> cir) {
+    if (SkyBlockSettings.saplingsDieOnSand && saplingIsOnSand(world, pos)) {
+      cir.setReturnValue(false);
     }
   }
 }
