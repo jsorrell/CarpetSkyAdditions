2a094b90aa239190ebae0d03b2aab14d711d31ca
Jack Sorrell <jack@jacksorrell.com>
2022-05-07 23:05:58 -0400

    Lower visibility of SkyBlockSetting

--- src/main/java/com/jsorrell/skyblock/settings/SkyBlockSetting.java
+++ src/main/java/com/jsorrell/skyblock/settings/SkyBlockSetting.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface SkyBlockSetting {
+@interface SkyBlockSetting {
   String value() default "";
 
   Class<? extends SettingFixer>[] fixer() default {};
