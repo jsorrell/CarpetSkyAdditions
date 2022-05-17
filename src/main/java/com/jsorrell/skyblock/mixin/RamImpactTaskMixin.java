c963bdf2eadf1b2deaa86a3390c6b63685a11af3
Jack Sorrell <jack@jacksorrell.com>
2022-05-17 15:11:05 -0400

    Change implementation of Rams breaking Nether Wart Blocks to mimic Rams dropping horns

--- src/main/java/com/jsorrell/skyblock/mixin/RamImpactTaskMixin.java
+++ src/main/java/com/jsorrell/skyblock/mixin/RamImpactTaskMixin.java
@@ -3,7 +3,6 @@ package com.jsorrell.skyblock.mixin;
 import com.jsorrell.skyblock.settings.SkyBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.ai.brain.Brain;
 import net.minecraft.entity.ai.brain.MemoryModuleState;
 import net.minecraft.entity.ai.brain.MemoryModuleType;
 import net.minecraft.entity.ai.brain.task.RamImpactTask;
@@ -14,24 +13,33 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.event.GameEvent;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.Iterator;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
 
-// TODO rework this once the goat horn from ramming implementation is completed
 @Mixin(RamImpactTask.class)
 public abstract class RamImpactTaskMixin<E extends PathAwareEntity> extends Task<E> {
+
+  @Shadow
+  @Final
+  private Function<GoatEntity, SoundEvent> impactSoundFactory;
+
+  @Shadow
+  protected abstract void finishRam(ServerWorld world, GoatEntity goat);
 
   public RamImpactTaskMixin(Map<MemoryModuleType<?>, MemoryModuleState> requiredMemoryState) {
     super(requiredMemoryState);
@@ -43,41 +51,33 @@ public abstract class RamImpactTaskMixin<E extends PathAwareEntity> extends Task
     at =
     @At(
       value = "INVOKE",
-      target =
-        "Lnet/minecraft/entity/ai/brain/task/RamImpactTask;finishRam(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/passive/GoatEntity;)V",
-      ordinal = 2))
+      target = "Lnet/minecraft/entity/ai/brain/Brain;getOptionalMemory(Lnet/minecraft/entity/ai/brain/MemoryModuleType;)Ljava/util/Optional;",
+      ordinal = 0),
+    cancellable = true)
   private void breakOpenNetherWart(ServerWorld world, GoatEntity rammer, long time, CallbackInfo ci) {
-    if (SkyBlockSettings.rammingWart) {
-      Brain<?> ramBrain = rammer.getBrain();
-      if (ramBrain.getOptionalMemory(MemoryModuleType.WALK_TARGET).isEmpty()
-        && ramBrain.getOptionalMemory(MemoryModuleType.RAM_TARGET).isPresent()
-        && world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) {
-        Box box = rammer.getBoundingBox().expand(0.05D);
+    if (SkyBlockSettings.rammingWart && world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) {
+      Optional<BlockPos> optionalWartPos = shouldBreakNetherWart(world, rammer);
+      if (optionalWartPos.isPresent()) {
+        BlockPos wartPos = optionalWartPos.get();
+        world.playSoundFromEntity(null, rammer, this.impactSoundFactory.apply(rammer), SoundCategory.HOSTILE, 1.0f, 1.0f);
 
-        Iterator<BlockPos> blocksHit =
-          BlockPos.iterate(
-              MathHelper.floor(box.minX),
-              MathHelper.floor(box.minY),
-              MathHelper.floor(box.minZ),
-              MathHelper.floor(box.maxX),
-              MathHelper.floor(box.maxY),
-              MathHelper.floor(box.maxZ))
-            .iterator();
-
-        blocksHit.forEachRemaining(
-          wartPos -> {
-            if (world.getBlockState(wartPos).isOf(Blocks.NETHER_WART_BLOCK)) {
-              Block.dropStack(
-                world, wartPos, new ItemStack(Items.NETHER_WART, world.random.nextInt(2) + 1));
-
-              boolean blockRemoved = world.removeBlock(wartPos, false);
-              if (blockRemoved) {
-                world.emitGameEvent(rammer, GameEvent.BLOCK_DESTROY, wartPos);
-                world.playSound(null, wartPos.getX(), wartPos.getY(), wartPos.getZ(), SoundEvents.BLOCK_WART_BLOCK_BREAK, SoundCategory.BLOCKS, 1.0f, 1.0f);
-              }
-            }
-          });
+        boolean blockRemoved = world.removeBlock(wartPos, false);
+        if (blockRemoved) {
+          if (!world.isClient()) {
+            Block.dropStack(world, wartPos, new ItemStack(Items.NETHER_WART, world.random.nextInt(2) + 1));
+          }
+          world.emitGameEvent(rammer, GameEvent.BLOCK_DESTROY, wartPos);
+          world.playSound(null, wartPos, SoundEvents.BLOCK_WART_BLOCK_BREAK, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        }
+        this.finishRam(world, rammer);
+        ci.cancel();
       }
     }
+  }
+
+  private Optional<BlockPos> shouldBreakNetherWart(ServerWorld world, GoatEntity goat) {
+    Vec3d movementVector = goat.getVelocity().multiply(1.0, 0.0, 1.0).normalize();
+    BlockPos hitPos = new BlockPos(goat.getPos().add(movementVector));
+    return world.getBlockState(hitPos).isOf(Blocks.NETHER_WART_BLOCK) ? Optional.of(hitPos) : Optional.empty();
   }
 }
