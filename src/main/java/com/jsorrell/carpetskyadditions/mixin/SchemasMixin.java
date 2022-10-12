ff2ff789e76885a520e0af9389587cb79d2ebf35
Jack Sorrell <jack@jacksorrell.com>
2022-10-12 19:35:53 -0400

    Fix issue where 1.19 still uses old generator name.

--- src/main/java/com/jsorrell/carpetskyadditions/mixin/SchemasMixin.java
+++ src/main/java/com/jsorrell/carpetskyadditions/mixin/SchemasMixin.java
@@ -1,9 +1,7 @@
 package com.jsorrell.carpetskyadditions.mixin;
 
 import com.jsorrell.carpetskyadditions.datafixer.Schema3079;
-import com.jsorrell.carpetskyadditions.datafixer.Schema3106;
 import com.jsorrell.carpetskyadditions.datafixer.SkyBlockGeneratorNameFix;
-import com.jsorrell.carpetskyadditions.datafixer.SkyBlockGeneratorNameFix2;
 import com.mojang.datafixers.DataFixerBuilder;
 import com.mojang.datafixers.schemas.Schema;
 import net.minecraft.datafixer.Schemas;
@@ -23,16 +21,5 @@ public abstract class SchemasMixin {
   private static void addSkyBlockGeneratorNameFix(DataFixerBuilder builder, CallbackInfo ci) {
     Schema schema3079 = builder.addSchema(3079, 1, Schema3079::new);
     builder.addFixer(new SkyBlockGeneratorNameFix(schema3079));
-  }
-
-  @Inject(
-    method = "build",
-    at = @At(value = "INVOKE",
-      target = "Lcom/mojang/datafixers/DataFixerBuilder;addFixer(Lcom/mojang/datafixers/DataFix;)V",
-      ordinal = 241)
-  )
-  private static void addSkyBlockGeneratorNameFix2(DataFixerBuilder builder, CallbackInfo ci) {
-    Schema schema3106 = builder.addSchema(3106, Schema3106::new);
-    builder.addFixer(new SkyBlockGeneratorNameFix2(schema3106));
   }
 }
