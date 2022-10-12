1c1d39d5c36bf5d608316ac1bb9d12ef105d7774
Jack Sorrell <jack@jacksorrell.com>
2022-10-12 15:35:53 -0400

    Fix #61 where wall coral fans didn't schedule ticks on block update

---
+++ src/main/java/com/jsorrell/carpetskyadditions/mixin/DeadCoralWallFanBlockMixin.java
@@ -0,0 +1,28 @@
+package com.jsorrell.carpetskyadditions.mixin;
+
+import com.jsorrell.carpetskyadditions.helpers.DeadCoralToSandHelper;
+import com.jsorrell.carpetskyadditions.settings.SkyAdditionsSettings;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.DeadCoralFanBlock;
+import net.minecraft.block.DeadCoralWallFanBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.WorldAccess;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DeadCoralWallFanBlock.class)
+public class DeadCoralWallFanBlockMixin extends DeadCoralFanBlock {
+  public DeadCoralWallFanBlockMixin(Settings settings) {
+    super(settings);
+  }
+
+  @Inject(method = "getStateForNeighborUpdate", at = @At(value = "HEAD"))
+  private void scheduleTickOnBlockUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos, CallbackInfoReturnable<BlockState> cir) {
+    if (SkyAdditionsSettings.coralErosion) {
+      world.createAndScheduleBlockTick(pos, this, DeadCoralToSandHelper.getSandDropDelay(world.getRandom()));
+    }
+  }
+}
