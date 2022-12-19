dd4c036761b956e358c38c3c67ff14ede07ff5d4
Jack Sorrell <jack@jacksorrell.com>
2022-12-19 14:14:55 -0500

    Revert "Fix the extended death time of coral fans caused partly by the fix of jsorrell/CarpetSkyAdditions#61 (#75)"
    This reverts commit 7d2e5aba9088f9bf3927608bce59eb7508b3891f.

--- src/main/java/com/jsorrell/carpetskyadditions/mixin/DeadCoralMixin.java
+++ src/main/java/com/jsorrell/carpetskyadditions/mixin/DeadCoralMixin.java
@@ -3,7 +3,6 @@ package com.jsorrell.carpetskyadditions.mixin;
 import com.jsorrell.carpetskyadditions.helpers.DeadCoralToSandHelper;
 import com.jsorrell.carpetskyadditions.settings.SkyAdditionsSettings;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.CoralFanBlock;
 import net.minecraft.block.CoralParentBlock;
 import net.minecraft.block.DeadCoralBlock;
 import net.minecraft.block.DeadCoralFanBlock;
@@ -31,7 +30,7 @@ public abstract class DeadCoralMixin extends CoralParentBlock {
 
   @Override
   public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
-    if (SkyAdditionsSettings.coralErosion && !((CoralParentBlock) this instanceof CoralFanBlock)) {
+    if (SkyAdditionsSettings.coralErosion) {
       world.createAndScheduleBlockTick(pos, this, DeadCoralToSandHelper.getSandDropDelay(world.getRandom()));
     }
     return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
