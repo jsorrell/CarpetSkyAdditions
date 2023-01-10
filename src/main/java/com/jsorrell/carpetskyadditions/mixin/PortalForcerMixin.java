630235e42653e3e6a6cd79880bdcfd1999901619
Jack Sorrell <jack@jacksorrell.com>
2023-01-10 01:04:11 -0500

    Make netherrack/nylium only generate with forced portals

--- src/main/java/com/jsorrell/carpetskyadditions/mixin/PortalForcerMixin.java
+++ src/main/java/com/jsorrell/carpetskyadditions/mixin/PortalForcerMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockLocating;
 import net.minecraft.world.PortalForcer;
 import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.border.WorldBorder;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -34,9 +35,13 @@ public class PortalForcerMixin {
     return Blocks.NETHERRACK.getDefaultState();
   }
 
-  @Inject(method = "createPortal", at = @At(value = "CONSTANT", args = "intValue=-1", ordinal = 4), locals = LocalCapture.CAPTURE_FAILSOFT)
-  private void addNetherrack(BlockPos pos, Direction.Axis axis, CallbackInfoReturnable<Optional<BlockLocating.Rectangle>> cir, Direction direction, double d, BlockPos blockPos) {
+  @Inject(method = "createPortal", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Direction;rotateYClockwise()Lnet/minecraft/util/math/Direction;", ordinal = 0), locals = LocalCapture.CAPTURE_FAILSOFT)
+  private void addNetherrack(BlockPos pos, Direction.Axis axis, CallbackInfoReturnable<Optional<BlockLocating.Rectangle>> cir, Direction direction, double d, BlockPos blockPos, double e, BlockPos blockPos2, WorldBorder worldBorder) {
     if (SkyAdditionsSettings.renewableNetherrack) {
+      if (!worldBorder.contains(blockPos)) {
+        return;
+      }
+
       BlockPos.Mutable mutablePos = new BlockPos.Mutable();
       Direction rotatedDirection = direction.rotateYClockwise();
       for (int i = -1; i < 3; ++i) { // i coordinate parallel to portal
