f465663c1b08a9714e963c7ad47bd640232e98fc
Jack Sorrell <jack@jacksorrell.com>
2023-01-11 13:53:27 -0500

    Remove possible weird side effects from selecting SkyBlock as the default world type

--- src/main/java/com/jsorrell/carpetskyadditions/gen/SkyAdditionsWorldPresets.java
+++ src/main/java/com/jsorrell/carpetskyadditions/gen/SkyAdditionsWorldPresets.java
@@ -1,18 +1,10 @@
 package com.jsorrell.carpetskyadditions.gen;
 
-import com.jsorrell.carpetskyadditions.config.SkyAdditionsConfig;
 import com.jsorrell.carpetskyadditions.util.SkyAdditionsIdentifier;
-import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.world.gen.WorldPreset;
-import net.minecraft.world.gen.WorldPresets;
 
 public class SkyAdditionsWorldPresets {
   public static final RegistryKey<WorldPreset> SKYBLOCK = RegistryKey.of(RegistryKeys.WORLD_PRESET, new SkyAdditionsIdentifier("skyblock"));
-
-  public static RegistryKey<WorldPreset> getInitiallySelectedPreset() {
-    SkyAdditionsConfig config = AutoConfig.getConfigHolder(SkyAdditionsConfig.class).get();
-    return config.defaultToSkyBlockWorld ? SkyAdditionsWorldPresets.SKYBLOCK : WorldPresets.DEFAULT;
-  }
 }
--- src/main/java/com/jsorrell/carpetskyadditions/mixin/CreateWorldScreenMixin.java
+++ src/main/java/com/jsorrell/carpetskyadditions/mixin/CreateWorldScreenMixin.java
@@ -1,11 +1,17 @@
 package com.jsorrell.carpetskyadditions.mixin;
 
+import com.jsorrell.carpetskyadditions.config.SkyAdditionsConfig;
 import com.jsorrell.carpetskyadditions.gen.SkyAdditionsWorldPresets;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
+import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.world.dimension.DimensionOptionsRegistryHolder;
 import net.minecraft.world.gen.WorldPreset;
+import net.minecraft.world.gen.WorldPresets;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,9 +21,18 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(CreateWorldScreen.class)
 public class CreateWorldScreenMixin {
   @Redirect(method = "create(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/gui/screen/Screen;)V", at = @At(value = "FIELD", opcode = Opcodes.GETSTATIC, target = "Lnet/minecraft/world/gen/WorldPresets;DEFAULT:Lnet/minecraft/registry/RegistryKey;"))
-  // This only selects the correct button -- it doesn't set the correct initial world gen settings
-  // See WorldPresetsMixin for that
-  private static RegistryKey<WorldPreset> setDefaultWorldPreset() {
-    return SkyAdditionsWorldPresets.getInitiallySelectedPreset();
+  private static RegistryKey<WorldPreset> setDefaultSelectedWorldPreset() {
+    SkyAdditionsConfig config = AutoConfig.getConfigHolder(SkyAdditionsConfig.class).get();
+    return config.defaultToSkyBlockWorld ? SkyAdditionsWorldPresets.SKYBLOCK : WorldPresets.DEFAULT;
+  }
+
+  @Redirect(method = "method_45686", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/WorldPresets;createDemoOptions(Lnet/minecraft/registry/DynamicRegistryManager;)Lnet/minecraft/world/dimension/DimensionOptionsRegistryHolder;"))
+  private static DimensionOptionsRegistryHolder setDefaultWorldGenSettings(DynamicRegistryManager drm) {
+    SkyAdditionsConfig config = AutoConfig.getConfigHolder(SkyAdditionsConfig.class).get();
+    if (config.defaultToSkyBlockWorld) {
+      return drm.get(RegistryKeys.WORLD_PRESET).entryOf(SkyAdditionsWorldPresets.SKYBLOCK).value().createDimensionsRegistryHolder();
+    } else {
+      return WorldPresets.createDemoOptions(drm);
+    }
   }
 }
