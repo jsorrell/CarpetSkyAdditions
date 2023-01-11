c351d2cd3817216ca1a4c4ca8393277b8471c0c3
Jack Sorrell <jack@jacksorrell.com>
2023-01-11 15:41:25 -0500

    Make the config read from the file when a new world is created

--- src/main/java/com/jsorrell/carpetskyadditions/mixin/CreateWorldScreenMixin.java
+++ src/main/java/com/jsorrell/carpetskyadditions/mixin/CreateWorldScreenMixin.java
@@ -6,6 +6,8 @@ import com.mojang.datafixers.util.Pair;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.registry.DynamicRegistryManager;
@@ -17,11 +19,19 @@ import net.minecraft.world.gen.WorldPresets;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(value = EnvType.CLIENT)
 @Mixin(CreateWorldScreen.class)
 public class CreateWorldScreenMixin {
+  // Try to reload config whenever Create New World is clicked
+  @Inject(method = "create(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/gui/screen/Screen;)V", at = @At(value = "HEAD"))
+  private static void loadConfigFromFile(MinecraftClient client, Screen parent, CallbackInfo ci) {
+    AutoConfig.getConfigHolder(SkyAdditionsConfig.class).load();
+  }
+
   @Redirect(method = "create(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/gui/screen/Screen;)V", at = @At(value = "FIELD", opcode = Opcodes.GETSTATIC, target = "Lnet/minecraft/world/gen/WorldPresets;DEFAULT:Lnet/minecraft/util/registry/RegistryKey;"))
   private static RegistryKey<WorldPreset> setDefaultSelectedWorldPreset() {
     SkyAdditionsConfig config = AutoConfig.getConfigHolder(SkyAdditionsConfig.class).get();
