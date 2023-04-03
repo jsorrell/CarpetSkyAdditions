c1d15b8f370938e2c75fa3ddca0998ee1602ffc0
Jack Sorrell <jack@jacksorrell.com>
2023-04-03 01:45:07 -0400

    Update buildscript and add changelog

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,11 @@ val versions = Versions(project.extra)
 val modId = project.extra["mod_id"] as String
 
 plugins {
-  id("fabric-loom").version("1.1-SNAPSHOT")
-  id("com.diffplug.spotless") version "6.17.0"
+  id("fabric-loom") version "1.1-SNAPSHOT"
+  id("com.diffplug.spotless") version "latest.release"
+  id("io.github.juuxel.loom-quiltflower") version "latest.release"
+  id("org.jetbrains.changelog") version "latest.release"
+  id("com.modrinth.minotaur") version "latest.release"
 }
 
 base {
@@ -52,6 +55,11 @@ tasks {
     options.release.set(versions.java.ordinal + 1)
   }
 
+  withType<AbstractArchiveTask> {
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+  }
+
   processResources {
     val templateContext = mapOf(
       "name" to project.extra["mod_name"],
@@ -69,6 +77,7 @@ tasks {
     toolchain { languageVersion.set(JavaLanguageVersion.of(versions.java.ordinal + 1)) }
     sourceCompatibility = versions.java
     targetCompatibility = versions.java
+    withSourcesJar()
   }
   jar {
     // Embed license in output jar
@@ -117,7 +126,7 @@ dependencies {
   modImplementation("com.terraformersmc", "modmenu", versions.modmenu)
 }
 
-tasks["assemble"].dependsOn(
+tasks.assemble.get().dependsOn(
   tasks.register<Zip>("zipTranslationPack") {
     description = "Zips the Translations resource pack"
     val tempDir = layout.buildDirectory.dir("translations-pack")
@@ -132,7 +141,6 @@ tasks["assemble"].dependsOn(
       }
     }
     archiveClassifier.set("translations")
-    archiveVersion.set(versions.mod)
     group = "build"
     from(tempDir)
     destinationDirectory.set(base.distsDirectory)
@@ -145,7 +153,6 @@ tasks["assemble"].dependsOn(
       from("src/main/resources/resourcepacks/skyblock")
     }
     archiveClassifier.set("datapack")
-    archiveVersion.set(versions.mod)
     group = "build"
     from(tempDir)
     destinationDirectory.set(base.distsDirectory)
@@ -182,4 +189,43 @@ spotless {
     target("**/*.yml", "**/*.yaml")
     (jackson() as JacksonYamlGradleConfig).yamlFeature("WRITE_DOC_START_MARKER", false)
   }
+
+  changelog {
+    version.set(versions.mod)
+    repositoryUrl.set(project.extra["repository"] as String)
+    introduction.set(
+      """
+        All notable changes to this project will be documented in this file.
+
+        The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/).
+      """.trimIndent(),
+    )
+    groups.set(listOf("Added", "Changed", "Deprecated", "Removed", "Fixed"))
+
+    // Curseforge markdown doesn't recognize "-"
+    itemPrefix.set("*")
+  }
+
+  modrinth {
+    token.set(providers.environmentVariable("MODRINTH_TOKEN"))
+    projectId.set("carpet-sky-additions")
+    versionNumber.set(providers.gradleProperty("mod_version"))
+    versionName.set("${versions.mod} for Minecraft ${versions.minecraft}")
+    versionType.set("release")
+    uploadFile.set(tasks.remapJar)
+    gameVersions.add(versions.minecraft)
+    loaders.add("fabric")
+    changelog.set(
+      provider {
+        project.changelog.renderItem(project.changelog.get(versions.mod).withHeader(false).withEmptySections(false).withLinks(false))
+      },
+    )
+    dependencies {
+      required.project("fabric-api")
+      required.project("carpet")
+      required.project("cloth-config")
+      optional.project("modmenu")
+    }
+  }
+  tasks.modrinth.get().dependsOn(tasks.patchChangelog)
 }
