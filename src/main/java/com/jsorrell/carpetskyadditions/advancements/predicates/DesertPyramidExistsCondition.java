a464bece32a3c9e268ce17fdd62e4d0373c03dcb
Jack Sorrell <jack@jacksorrell.com>
2023-07-30 21:18:09 -0400

    Improve DesertPyramidExistsCondition debugging

--- src/main/java/com/jsorrell/carpetskyadditions/advancements/predicates/DesertPyramidExistsCondition.java
+++ src/main/java/com/jsorrell/carpetskyadditions/advancements/predicates/DesertPyramidExistsCondition.java
@@ -2,13 +2,14 @@ package com.jsorrell.carpetskyadditions.advancements.predicates;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.gson.*;
-import com.jsorrell.carpetskyadditions.settings.SkyAdditionsSettings;
 import com.jsorrell.carpetskyadditions.util.SkyAdditionsResourceLocation;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.world.entity.Entity;
@@ -16,15 +17,18 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParam;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
+import net.minecraft.world.phys.AABB;
 
 public class DesertPyramidExistsCondition implements LootItemCondition {
-    static List<Block> BLOCKS_TO_CHECK = List.of(
+    static final List<Block> BLOCKS_TO_CHECK = List.of(
             Blocks.BLUE_TERRACOTTA,
             Blocks.ORANGE_TERRACOTTA,
             Blocks.SANDSTONE,
@@ -74,12 +78,40 @@ public class DesertPyramidExistsCondition implements LootItemCondition {
                 List<StructureTemplate.StructureBlockInfo> requiredBlocks =
                         template.filterBlocks(structureOrigin, placeSettings, block);
                 for (StructureTemplate.StructureBlockInfo requiredBlock : requiredBlocks) {
-                    if (requiredBlock.state() != level.getBlockState(requiredBlock.pos())) {
+                    BlockState requiredState = requiredBlock.state();
+                    BlockState currentState = level.getBlockState(requiredBlock.pos());
+                    if (currentState != requiredState) {
                         // Use terracotta to determine location and rotation
-                        if (!requiredBlock.state().is(BlockTags.TERRACOTTA)) {
-                            // Help people debug builds
-                            SkyAdditionsSettings.LOG.warn(
-                                    "Incorrect Block at " + requiredBlock.pos() + " in Desert Pyramid");
+                        if (!requiredState.is(BlockTags.TERRACOTTA)) {
+                            // Help players within 10 blocks of bounding box debug builds
+                            AABB buildersBox = AABB.of(template.getBoundingBox(placeSettings, structureOrigin)
+                                    .inflatedBy(10));
+                            List<ServerPlayer> playersToNotify = serverLevel.getPlayers(
+                                    serverPlayer -> buildersBox.contains(serverPlayer.position()));
+                            MutableComponent message;
+                            if (currentState.getBlock() == requiredState.getBlock()) {
+                                Map.Entry<Property<?>, Comparable<?>> incorrectProperty =
+                                        requiredState.getValues().entrySet().stream()
+                                                .filter(e -> currentState.getValue(e.getKey()) != e.getValue())
+                                                .findAny()
+                                                .orElseThrow();
+                                message = Component.translatable(
+                                        "message.desert_pyramid_incorrect_state",
+                                        requiredBlock.pos().getX(),
+                                        requiredBlock.pos().getY(),
+                                        requiredBlock.pos().getZ(),
+                                        incorrectProperty.getKey().getName(),
+                                        incorrectProperty.getValue());
+                            } else {
+                                message = Component.translatable(
+                                        "message.desert_pyramid_incorrect_block",
+                                        requiredBlock.pos().getX(),
+                                        requiredBlock.pos().getY(),
+                                        requiredBlock.pos().getZ(),
+                                        requiredState.getBlock().getName());
+                            }
+                            playersToNotify.forEach(
+                                    player -> player.sendSystemMessage(message.withStyle(ChatFormatting.DARK_RED)));
                         }
                         return false;
                     }
